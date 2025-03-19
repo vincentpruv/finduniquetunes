9c5af64ad9c793db3451f6f3fe2532ee1e7462c2
Vincent Pruvost <vincent@MacBook-Air-de-Vincent.local>
2025-03-19 19:56:18 +0100

    bye bye snake game (wont regret u)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Snake from "./pages/Snake";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -20,7 +19,6 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/snake" element={<Snake />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
